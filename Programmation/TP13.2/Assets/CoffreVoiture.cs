f52a9fdd069765d0df918b621cf740f32b51b90f
CallHector <chris.banzet@gmail.com>
2025-11-23 01:31:25 +0100

    Prog TD13 part 2

---
+++ Programmation/TP13.2/Assets/CoffreVoiture.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class CoffreVoiture
+    {
+        private float capacite;
+        //définition du constructeur par défaut
+        public CoffreVoiture()
+        {
+            Debug.Log("Le coffre de la voiture a été créé ");
+            capacite = 0;
+        }
+        //définition du constructeur alternatif
+        public CoffreVoiture(float capaciteCoffre)
+        {
+            capacite = capaciteCoffre;
+            Debug.Log("Coffre de la voiture a été créé avec l’argument suivant : " + capaciteCoffre);
+        }
+         ~CoffreVoiture()
+         { //définition du destructeur
+         Debug.Log("Le coffre de la voiture sera maintenant détruit!");
+         }
+
+        // Start is called before the first frame update
+       
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+          
+        }
+    }
